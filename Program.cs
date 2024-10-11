0032a7399be23bbbba6922386e5a615ab943391f
Dihtarenko324 <arara@gmail.com>
2024-10-11 08:52:34 +0300

    Hello world!

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int a, b;
+
+        }
+    }
+}
